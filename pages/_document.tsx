d39234a382ab7800802f69fc975d1b31de04de38
Belkin <99342964+Belkin1337@users.noreply.github.com>
2023-12-05 21:17:10 +0300

    added yandex metrics, Image components instead <img/>

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -14,11 +14,8 @@ export default function Document() {
           <Main />
           <NextScript />
           <Partytown debug={true} forward={['dataLayer.push']} />
-          <Script
-            id="metrika-counter" 
-            strategy="afterInteractive"
-            dangerouslySetInnerHTML={{ 
-              __html: `(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+          <Script id="metrika-counter" strategy="afterInteractive">
+            {`(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
               m[i].l=1*new Date();
               for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
               k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
@@ -28,10 +25,8 @@ export default function Document() {
                    clickmap:true,
                    trackLinks:true,
                    accurateTrackBounce:true
-              });
-              `,
-            }}
-          />
+            });`}
+          </Script>
           <noscript>
             <div>
               <img
