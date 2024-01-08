71cf07c26a2d28cf139282e838135d8557f293ee
Belkin <rusik.normov@bk.ru>
2024-01-08 16:42:36 +0300

    .

--- pages/status/index.tsx
+++ pages/status/index.tsx
@@ -31,7 +31,7 @@ export default function Status(props: { status: Status }) {
   return (
     <div className="flex items-center min-h-screen py-36 bg-cover bg-no-repeat" style={{ backgroundImage: `url("/images/alex_in_cave.jpg")` }}>
       <div className="absolute top-0 right-0 left-0 bg-gradient-to-r from-black/80 from-70% to-transparent min-h-screen group-hover:transition group-hover:duration-500 transition duration-500" />
-      <div className="relative flex flex-col lg:flex-row items-center justify-start -right-24 h-full w-[90%] mx-auto gap-x-12 gap-y-6">
+      <div className="relative flex flex-col lg:flex-row items-center justify-start lg:-right-24 h-full w-[90%] mx-auto gap-x-12 gap-y-6">
         <div className="flex flex-col rounded-xl p-1 ">
           <div className="flex h-[48px] items-center p-3 bg-neutral-900/80">
             <p className="text-bisquite-server-color text-2xl cursor-pointer">
