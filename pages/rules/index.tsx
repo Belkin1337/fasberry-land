b3d258f44a52d1f3c3c377832f21b38f17ee1ef1
Belkin <rusik.normov@bk.ru>
2024-02-11 16:14:29 +0300

    fixed mainlayout component

--- pages/rules/index.tsx
+++ pages/rules/index.tsx
@@ -11,26 +11,26 @@ export default function RulesPage() {
       <Head>
         <title>Fasberry &gt; Правила</title>
       </Head>
-      <div className="full-screen-section flex items-center justify-start bg-bottom md:bg-center"
-        style={{
-          backgroundImage: `url("https://cdn.discordapp.com/attachments/904344676587417621/1197634958408290406/minecraft_screen_13.webp?ex=65bbfb79&is=65a98679&hm=4ab397c0c45719831c9d9a56d3cf8f38aba84aa3c8a0fe47697cb841b7722366&")`
-        }}>
-        <Overlay variant="default" />
-        <WrapperTitle>
-          <div className="flex flex-col w-full lg:max-w-xl items-start justify-center">
-            <div className="bg-black/80 rounded-xl p-4 lg:p-6">
-              <Typography position="left" shadow="xl" className="text-2xl lg:text-3xl xl:text-4xl text-gold mb-2">
-                Правила
-              </Typography>
-              <Typography position="left" className="text-xl xl:text-2xl">
-                Правила созданы для чего? Правильно, чтобы их не читать.
-              </Typography>
-            </div>
-          </div>
-        </WrapperTitle>
-      </div>
       <MainLayoutPage variant="with_section">
-        <RulesList />
+        <div className="full-screen-section flex items-center justify-start bg-bottom md:bg-center"
+          style={{
+            backgroundImage: `url("https://cdn.discordapp.com/attachments/904344676587417621/1197634958408290406/minecraft_screen_13.webp?ex=65bbfb79&is=65a98679&hm=4ab397c0c45719831c9d9a56d3cf8f38aba84aa3c8a0fe47697cb841b7722366&")`
+          }}>
+          <Overlay variant="default" />
+          <WrapperTitle>
+            <div className="flex flex-col w-full lg:max-w-xl items-start justify-center">
+              <div className="bg-black/80 rounded-xl p-4 lg:p-6">
+                <Typography position="left" shadow="xl" className="text-2xl lg:text-3xl xl:text-4xl text-gold mb-2">
+                  Правила
+                </Typography>
+                <Typography position="left" className="text-xl xl:text-2xl">
+                  Правила созданы для чего? Правильно, чтобы их не читать.
+                </Typography>
+              </div>
+            </div>
+          </WrapperTitle>
+        </div >
+      <RulesList />
       </MainLayoutPage>
     </>
   )
